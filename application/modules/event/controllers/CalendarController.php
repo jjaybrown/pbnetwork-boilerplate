d4c6ca44e065427c27a9c6ca479c008aa2f62142
Jason Brown <j.brown@ctidigital.com>
2012-02-16 18:00:08 +0000

    formatted calendar view and added custom find method for events

--- application/modules/event/controllers/CalendarController.php
+++ application/modules/event/controllers/CalendarController.php
@@ -11,9 +11,16 @@ class Event_CalendarController extends Zend_Controller_Action
 {
     
     private $_calendarService = null;
+    private $_events = null;
+    
+     /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $_em = null;
     
     public function init()
     {
+        $this->_em = Zend_Registry::get('em');
         $this->_calendarService = new \App\Service\Calendar();
         $this->_calendarService->setValidDates(0, 11); //11 months back, 4 months forward
     }
@@ -22,7 +29,21 @@ class Event_CalendarController extends Zend_Controller_Action
     {
         // Build the calendar header data
         $this->view->monthHeader = $this->_calendarService->getFocusDate()->get("MMMM yyyy");
-        $this->view->calHeader = $this->_calendarService->getCalendarHeaderDataArray();
+        $this->view->previousMonth = array(
+            'controller' => 'calendar',
+            'action' => 'view',
+            'month' => $this->_calendarService->getPrevMonth()->get("M"),
+            'year' => $this->_calendarService->getPrevMonth()->get("yyyy")
+        );
+        
+        $this->view->nextMonth = array(
+            'controller' => 'calendar',
+            'action' => 'view',
+            'month' => $this->_calendarService->getNextMonth()->get("M"),
+            'year' => $this->_calendarService->getNextMonth()->get("yyyy")
+        );
+        
+        //$this->view->calHeader = $this->_calendarService->getCalendarHeaderDataArray();
         
          // Build the calendar weekdays data
         $this->view->calWeekdays = $this->_calendarService->getCalendarWeekdayDataArray();
@@ -41,17 +62,43 @@ class Event_CalendarController extends Zend_Controller_Action
         $month = $request->getParam('month', date('F'));
         $year = $request->getParam('year', date('Y'));
 
-        // CalendarService
+        // Set focus date on calendar service
         $this->_calendarService->setFocusDate("$month $year");
         
         // Build the calendar header data
         $this->view->monthHeader = $this->_calendarService->getFocusDate()->get("MMMM yyyy");
-        $this->view->calHeader = $this->_calendarService->getCalendarHeaderDataArray();
+        $this->view->previousMonth = array(
+            'controller' => 'calendar',
+            'action' => 'view',
+            'month' => $this->_calendarService->getPrevMonth()->get("M"),
+            'year' => $this->_calendarService->getPrevMonth()->get("yyyy")
+        );
+        
+        $this->view->nextMonth = array(
+            'controller' => 'calendar',
+            'action' => 'view',
+            'month' => $this->_calendarService->getNextMonth()->get("M"),
+            'year' => $this->_calendarService->getNextMonth()->get("yyyy")
+        );
+        
+        //$this->view->calHeader = $this->_calendarService->getCalendarHeaderDataArray();
         
          // Build the calendar weekdays data
         $this->view->calWeekdays = $this->_calendarService->getCalendarWeekdayDataArray();
 
         // Build the calendar monthdays data
         $this->view->calMonthDays = $this->_calendarService->getCalendarMonthDayDataArray();
+        $this->_getEventsForFocusedMonth($this->_calendarService->getFocusDate());
+    }
+    
+    protected function _getEventsForFocusedMonth(\Zend_Date $focus = null)
+    {
+        // Check if a focused month has been provided
+        if(is_null($focus)){
+            $focus = $this->_calendarService->getFocusDate();
+        }
+        
+        $data = $this->_em->getRepository("\App\Entity\Event")->findByEventStartDate($focus);
+        Zend_Debug::dump($data);
     }
 }
--- library/App/Repository/Event.php
+++ library/App/Repository/Event.php
@@ -10,4 +10,12 @@ class Event extends EntityRepository
         $stmt = 'SELECT e FROM App\Entity\Event e ORDER BY e._id DESC';
         return $this->_em->createQuery($stmt)->getResult();
     }
+    
+    public function findByEventStartDate(\Zend_Date $startDate)
+    {
+        $month = $startDate->get('M');
+        $year = $startDate->get('yyyy');
+        $stmt = 'SELECT e FROM App\Entity\Event e WHERE MONTH(e.start) = '.$month.' AND YEAR(e.start) = '.$year.' ORDER BY e.id DESC';
+        return $this->_em->createQuery($stmt)->getResult();
+    }
 }
