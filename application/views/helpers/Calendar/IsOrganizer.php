4c7e10d66f3095f4ef84622196e313abdcfafaa6
Jason Brown <jason.brown@jbfreelance.co.uk>
2012-03-17 16:19:08 +0000

    setup organiser controls

--- application/views/helpers/Calendar/IsOrganizer.php
+++ application/views/helpers/Calendar/IsOrganizer.php
@@ -4,7 +4,16 @@ class Zend_View_Helper_IsOrganizer extends Zend_View_Helper_Abstract
 {
     public function IsOrganizer()
     {
+        $auth = Zend_Auth::getInstance();
         
+        // Check for logged in user
+        if($auth->hasIdentity())
+        {
+            // Does this user have access as an organizer
+            if($auth->getIdentity()->getRoleId() == "Organizer" || $auth->getIdentity()->getRoleId() == "Admin")
+            {
+            }
+        }
     }
 }
 ?>
