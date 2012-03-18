faa69eca5abb913527bfef45232ad57f5384152b
Jason Brown <jason.brown@jbfreelance.co.uk>
2012-03-18 13:48:06 +0000

    added order controller

---
+++ application/modules/admin/controllers/OrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Controller as AppController;
+
+class Admin_OrderController extends AppController
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+    
+    public function indexAction()
+    {
+        
+    }
+}
+?>
