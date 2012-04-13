ecb9b2b46908b6bd85063d79c8f4125c7b7ed6f7
Jason Brown <j.brown@ctidigital.com>
2012-04-13 17:15:40 +0100

    added magazine module

---
+++ application/modules/magazine/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+use App\Controller as AppController;
+
+class Magazine_IndexController extends AppController
+{
+    public function init()
+    {
+        parent::init();
+    }
+        
+    public function indexAction()
+    {
+    }
+
+}
